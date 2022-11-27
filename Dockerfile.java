ebe1c6f151a324ba944b6922d636e93e312d009e
AshuiRUA <58521933+AshuiRUA@users.noreply.github.com>
2022-11-27 19:08:35 +0800

    fix: dependancy error

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,7 +1,7 @@
-FROM openjdk:14
+FROM openjdk:17
 
 COPY . /home
 WORKDIR /home/java
-RUN curl -L -o dd-java-agent.jar 'https://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=com.datadoghq&a=dd-java-agent&v=0.69.0'
+RUN curl -L -o dd-java-agent.jar 'http://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=com.datadoghq&a=dd-java-agent&v=latest'
 RUN ./gradlew --no-daemon installDist
 CMD JAVA_OPTS="-Ddd.agent.host=$DD_AGENT_HOST -Ddd.profiling.enabled=true -javaagent:dd-java-agent.jar" ./build/install/movies-api-java/bin/movies-api-java
